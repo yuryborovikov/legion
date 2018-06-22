61479922c03a709935c431e70d8b0fcc7a7b49db
Andrii Bilorus <andrii.bilorus@gmail.com>
2018-06-22 14:33:09 +0300

    Deploy Jenkins with preconfigured openid plugin

--- k8s/jenkins/scripts/security.groovy
+++ k8s/jenkins/scripts/security.groovy
@@ -18,10 +18,15 @@
 import jenkins.model.*
 import hudson.security.*
 import jenkins.security.s2m.AdminWhitelistRule
+import hudson.model.*
+import hudson.tools.*
+import hudson.plugins.*
+import hudson.security.SecurityRealm.*
+import org.jenkinsci.plugins.oic.*
  
 def instance = Jenkins.getInstance()
- 
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+def env = System.getenv()
 
 if (hudsonRealm.getAllUsers().size == 0){
 	hudsonRealm.createAccount("admin", "admin")
@@ -33,6 +38,56 @@ if (hudsonRealm.getAllUsers().size == 0){
 	instance.save()
 	 
 	Jenkins.instance.getInjector().getInstance(AdminWhitelistRule.class).setMasterKillSwitch(false)
+}
+
+enableOpenId = env['OPENID_ENABLE']
+if (enableOpenId.toLowerCase() == "true") {
+  String clientId = env['OPENID_CLIENTID']
+  String clientSecret = env['OPENID_CLIENT_SECRET']
+  String tokenServerUrl = env['OPENID_TOKEN_SERVER_URL']
+  String authorizationServerUrl = env['OPENID_AUTH_SERVER_URL']
+  String userInfoServerUrl = ''
+  String userNameField = 'sub'
+  String tokenFieldToCheckKey = ''
+  String tokenFieldToCheckValue = ''
+  String fullNameFieldName = 'name'
+  String emailFieldName = 'email'
+  String scopes = 'openid email profile'
+  String groupsFieldName = ''
+  boolean disableSslVerification = 'false'
+  boolean logoutFromOpenidProvider = 'false'
+  String endSessionUrl = ''
+  String postLogoutRedirectUrl = ''
+  boolean escapeHatchEnabled = 'false'
+  String escapeHatchUsername = ''
+  String escapeHatchSecret = ''
+  String escapeHatchGroup = ''
+
+  adrealm = new OicSecurityRealm(
+    clientId,
+    clientSecret,
+    tokenServerUrl,
+    authorizationServerUrl,
+    userInfoServerUrl,
+    userNameField,
+    tokenFieldToCheckKey,
+    tokenFieldToCheckValue,
+    fullNameFieldName,
+    emailFieldName,
+    scopes,
+    groupsFieldName,
+    disableSslVerification,
+    logoutFromOpenidProvider,
+    endSessionUrl,
+    postLogoutRedirectUrl,
+    escapeHatchEnabled,
+    escapeHatchUsername,
+    escapeHatchSecret,
+    escapeHatchGroup
+  )
+
+  instance.setSecurityRealm(adrealm)
+  instance.save()
 }
 
 System.setProperty("hudson.model.DirectoryBrowserSupport.CSP", "")
