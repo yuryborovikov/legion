8ad1ed60c692ce05e1fea00f7a5496bd1f6605de
Kirill Makhonin <Kirill_Makhonin@epam.com>
2018-07-23 16:29:48 +0300

    [#250] Add draft of properties storage with tests

--- legion/tests/test_models/summation_model/run.py
+++ legion/tests/test_models/summation_model/run.py
@@ -1,10 +1,14 @@
 import legion.model
+import legion.k8s
 
 legion.model.init('test summation', '1.0')
 
 # py_model.properties['prop_1'] = 0.6
 # py_model.on_property_change(lambda key: print(key))
 
+secret_storage = K8SPropertyStorage('NAMEPSPACE', 'NAME')
+
+download_from_s3(sk_id=secret_storage['key1'])
 
 def calculate(x):
     # a = legion.model.properties['prop_1']
