8e3e8df23a37cda426b9faba29dff7968ba55a8c
Kirill Makhonin <Kirill_Makhonin@epam.com>
2018-07-24 15:20:23 +0300

    [#250] Update tests

--- legion/tests/test_models/summation_model/run.py
+++ legion/tests/test_models/summation_model/run.py
@@ -1,34 +1,10 @@
 import legion.model
-import legion.k8s
 
 legion.model.init('test summation', '1.0')
 
-<<<<<<< 63cede55a3f38705615175ecc6d66e2c7d8a7bcc
 # py_model.properties['prop_1'] = 0.6
 # py_model.on_property_change(lambda key: print(key))
-=======
-py_model = legion.pymodel.Model('test summation', '1.0')  # do the same shit as model.init does
-                                                          # and set legion.model.context
 
-import configparser
-cm = configparser.ConfigParser()
-
-
-get('key', cast=int)
-
-
-py_model.define_property('prop_1', 0.5)  # start-value is required
-# py_model.properties['prop_1'] = 0.5 ^^^^^^^^  (just initialization)
-
-
-py_model.on_property_change(lambda key: print(key))
-
-py_model.send_metrics('metric_a', 1.0)
->>>>>>> [#250] Update typing system, add tests
-
-secret_storage = K8SPropertyStorage('NAMEPSPACE', 'NAME')
-
-download_from_s3(sk_id=secret_storage['key1'])
 
 def calculate(x):
     # a = legion.model.properties['prop_1']
