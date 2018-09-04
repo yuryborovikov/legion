fafe096a68b111bc73265acebf37e73fd7a08ca3
Kirill Makhonin <kroks.rus@gmail.com>
2018-09-04 11:19:42 +0300

    [#406] Add tests

--- legion/tests/test_models/complex_model/run.py
+++ legion/tests/test_models/complex_model/run.py
@@ -1,3 +1,4 @@
+import os
 import legion.model
 
 import complex_package.submodule
@@ -6,6 +7,12 @@ legion.model.init('complex', '1.0')
 
 
 legion.model.define_property('prop_1', 0.5)
+
+FILE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data.dat'))
+
+# write file on train phase
+with open(FILE_PATH, 'w') as fstream:
+    fstream.write('10')
 
 
 def on_property_update():
@@ -16,6 +23,9 @@ legion.model.on_property_update(on_property_update)
 
 
 def calculate(x):
+    # check file availability on execution phase
+    assert os.path.exists(FILE_PATH), 'file not exists: {}'.format(FILE_PATH)
+
     print('I have a property {}'.format(legion.model.properties['prop_1']))
 
     return complex_package.submodule.add_42(int(x['value']))
